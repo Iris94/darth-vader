db81e7ea57eb42a4850ae3c674a022d3cb151f44
Iris94 <mirza.iris94@gmail.com>
2023-09-15 19:27:31 +0200

    Fixing lightsaber arm

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Head, Torso } from "@/components"
 export default function Home() {
   return (
     <main 
-    className="bg-white w-screen h-screen flex flex-col">
+    className="bg-white w-screen h-screen flex flex-col overflow-hidden">
       <Head />
       <Torso /> 
     </main>
--- components/SaberArm.tsx
+++ components/SaberArm.tsx
@@ -1,7 +1,15 @@
-
+import styles from './SaberArm.module.css';
 
 export default function SaberArm () {
     return (
-        <div></div>
+        <div 
+        className="w-full h-full flex justify-end items-center">
+            <div className={styles['right-arm-shape']}>
+                <div className={styles['right-arm']}></div>
+                <div className={styles['right-hand']}>
+                    <div className={styles['lightsaber-box']}></div>
+                </div>
+            </div>
+        </div>
     )
 }
