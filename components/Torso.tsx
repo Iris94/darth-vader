2ad1088e8d7a5ce52ba93b354b5dd51b2624b806
Iris94 <mirza.iris94@gmail.com>
2023-09-15 18:17:54 +0200

    Basic drawing

--- components/Torso.tsx
+++ components/Torso.tsx
@@ -4,7 +4,7 @@ import { SaberArm } from '.'
 export default function Torso () {
     return (
         <div className="w-full h-full flex flex-col items-center justify-start">
-            <div className="lg:w-2/5 w-4/5 h-2/3 flex flex-col items-center relative bottom-5 z-1">
+            <div className="lg:w-2/5 w-full h-4/5 flex flex-col items-center relative bottom-5 z-1">
                 <div className={styles["torso"]}>
                   <div className={styles['torso-top']}>
                         <div></div>
@@ -15,10 +15,51 @@ export default function Torso () {
                     </div>  
 
                   <div className={styles['torso-bottom']}>
-                    <div className={styles['left-arm']}></div>
-                    
-                    <div className={styles['armor']}></div>
-
+                    <div className={styles['left-arm']}>
+                        <div className={styles['left-arm-shape']}></div>
+                    </div>
+                    <div className={styles['armor']}>
+                        <div className={styles['armor-shape']}>
+                            <div className={styles['armor-top']}>
+                               <div className={styles['armor-top-line']}>
+                                <div className={styles['armor-above-cpu']}>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div className={styles['armor-cpu']}>
+                                    <div className={styles['armor-cpu-top']}>
+                                        <div className={styles['armor-cpu-top-left']}>
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <div className={styles['armor-cpu-top-right']}>
+                                            <div className={styles['green']}></div>
+                                            <div className={styles['red']}></div>
+                                        </div>
+                                    </div>
+                                    <div className={styles['armor-cpu-bottom']}>
+                                        <div></div>
+                                        <div></div>
+                                        <div></div>
+                                        <div></div>
+                                    </div>
+                                </div>
+                                <div className={styles['armor-bellow-cpu']}>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                               </div>
+                            </div>
+                            <div className={styles['armor-bottom']}>
+                               <div className={styles['belt']}></div> 
+                            </div>
+                        </div>
+                    </div>
                     <div className={styles['right-arm']}>
                         {<SaberArm />}
                     </div> 
